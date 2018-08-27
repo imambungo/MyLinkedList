88bca5800b663d3b27d01f09db8920282840d56c
M. Imam Pratama <imambungo@gmail.com>
2018-08-27 12:11:26 +0700

    LinkedList Class ready for first try

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,30 +1,44 @@
-class MyLinkedList{
+public class MyLinkedList{
     private int index=0;
     private int maxIndex=0;
     private int isiInt;
-    private boolean first = false;
+    private boolean firstAdd = false;
+    private boolean firstTampilkan = false;
     MyLinkedList next;
     public MyLinkedList(int isiInt, int index) {
         this.isiInt = isiInt;
         this.index = ++index;
         this.maxIndex = this.index;
     }
-    public boolean diUjung(){
+    public boolean diUjung() {
         if (index == maxIndex) {
             return true;
         }
         return false;
     }
     public void add(int ygDitambah) {
-        if (this.maxIndex == 0 && this.first == false) {
+        if (this.maxIndex == 0 && this.firstAdd == false) {
             this.isiInt = ygDitambah;
-            this.first = true;
+            this.firstAdd = true;
         }else{
             this.maxIndex++;
             if (diUjung()) {
                 next = new MyLinkedList(ygDitambah, this.index);
             } else {
                 next.add(ygDitambah);
+            }
+        }
+    }
+    public void tampilkan() {
+        if (this.maxIndex == 0 && this.firstTampilkan == false) {
+            System.out.print("["+this.isiInt+",");
+            this.firstTampilkan = true;
+        } else {
+            if (diUjung()) {
+                System.out.println(this.isiInt+"]");
+            } else {
+                System.out.println(this.isiInt+",");
+                next.tampilkan();
             }
         }
     }
