f17c1ebc07af3d1c967a6170d984bdeb1aaa68ed
M. Imam Pratama <imambungo@gmail.com>
2018-08-27 09:02:26 +0700

    Initial commit

---
+++ MyLinkedList.java
@@ -0,0 +1,13 @@
+class MyLinkedList{
+    private int index;
+    private int maxIndex;
+    private int isiInt=0;
+    public MyLinkedList(int isiInt){
+        
+    }
+    public void add(int ygDitambah){
+        if(diUjung()){
+            MyLinkedList next = new MyLinkedList(int isiInt);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Belajar Struktur Data
+
+Coba-coba bikin LinkedList sendiri.  
+Fitur:  
+-tambah elemen  
+-sisipkan elemen  
+-hapus elemen
