4b8d0bef75cbdce1334b70d4847dcb0a19647c40
M. Imam Pratama <imambungo@gmail.com>
2018-08-28 23:15:00 +0700

    Tambah fitur getBanyakElemen dan getElemenPadaIndexKe

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -11,12 +11,31 @@ public class MyLinkedList{
     }
     
     public void tampilkan() {
-            if (this.next == null) {
-                System.out.println("LinkedList ini kosong gan");
-            } else {
-                System.out.print("[");
-                next.tampilkan();
+        if (this.next == null) {
+            System.out.println("LinkedList ini kosong gan");
+        } else {
+            System.out.print("[");
+            next.tampilkan();
+        }
+    }
+
+    public int index(int index) {
+        if (this.next == null || index < 0) {
+            try {
+                throw new IndexOutOfBoundsException("Index ini tidak ada gan :v");
             }
+            catch (IndexOutOfBoundsException e) {
+                // System.out.println(e.getMessage());                
+            }
+        }
+        return next.index(index);
+    }
+
+    public int length() {
+        if (next == null) {
+            return 0;
+        }
+        return next.length();
     }
 }
 class Node{
@@ -44,5 +63,25 @@ class Node{
             System.out.print(this.isiInt + ",");
             next.tampilkan();
         }
+    }
+
+    public int index(int index) {
+        if(this.index == index) {
+            return this.isiInt;
+        }else if (this.next == null) {
+            try {
+                throw new IndexOutOfBoundsException("Index ini tidak ada gan :v");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return next.index(index);
+    }
+
+    public int length() {
+        if (next == null) {
+            return 1;
+        }
+        return 1 + next.length();
     }
 }
--- TestLinkedList.java
+++ TestLinkedList.java
@@ -12,8 +12,13 @@ class TestLinkedList{
         a.add(5);
         a.add(4);
         a.tampilkan();
-        // System.out.println(a.index(0));
-        // System.out.println(a.index(3));
-        // System.out.println(a.index(a.length()));
+        System.out.println(a.index(0));
+        System.out.println(a.index(3));
+        // System.out.println(a.index(-1));
+        // System.out.println(a.index(100));
+        System.out.println(a.index(a.length()-1));
+        for (int i = 0; i < a.length(); i++) {
+            System.out.print(a.index(i)+" ");
+        }
     }
 }
