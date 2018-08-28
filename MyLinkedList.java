3ecae3ff5606a165b268bc61b63c831d707e573e
M. Imam Pratama <imambungo@gmail.com>
2018-08-28 17:49:21 +0700

    class MyLinkedList jadi Head yg sebelumnya di MyLinkedList dipindah ke class Node.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,43 +1,57 @@
 public class MyLinkedList{
-    private int index=0;
-    private int maxIndex=0;
+    Node next;
+    
+    public void add(int ygDitambah) {
+        if (this.next == null) {
+            next = new Node(ygDitambah, 0);
+        } else {
+            next.add(ygDitambah);
+        }
+    }
+    
+    public void tampilkan() {
+        // if (this.index == 0) {
+        //     System.out.print("[" + this.isiInt + ",");
+        //     next.tampilkan();
+        // } else {
+            if (this.next == null) {
+                System.out.println("LinkedList ini kosong gan");
+            } else {
+                System.out.print("[");
+                next.tampilkan();
+            }
+        // }
+    }
+}
+class Node{
+    private int index;
     private int isiInt;
-    private boolean firstAdd = false;
-    MyLinkedList next;
-    public MyLinkedList(){}
-    public MyLinkedList(int isiInt, int index) {
+    Node next;
+
+    public Node(){}
+
+    public Node(int isiInt, int index) {
         this.isiInt = isiInt;
         this.index = ++index;
-        this.maxIndex = this.index;
     }
-    public boolean diUjung() {
-        if (this.next == null) {
-            return true;
-        }
-        return false;
-    }
+
     public void add(int ygDitambah) {
-        if (this.maxIndex == 0 && this.firstAdd == false) {
-            this.isiInt = ygDitambah;
-            this.firstAdd = true;
-        }else{
-            if (diUjung()) {
-                next = new MyLinkedList(ygDitambah, this.index);
-            } else {
-                next.add(ygDitambah);
-            }
-            this.maxIndex++;
+        if (this.next == null) {
+            next = new Node(ygDitambah, this.index);
+        } else {
+            next.add(ygDitambah);
         }
     }
+
     public void tampilkan() {
         if (this.index == 0) {
-            System.out.print("["+this.isiInt+",");
+            System.out.print("[" + this.isiInt + ",");
             next.tampilkan();
         } else {
-            if (diUjung()) {
-                System.out.println(this.isiInt+"]");
+            if (this.next == null) {
+                System.out.println(this.isiInt + "]");
             } else {
-                System.out.print(this.isiInt+",");
+                System.out.print(this.isiInt + ",");
                 next.tampilkan();
             }
         }
